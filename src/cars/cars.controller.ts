10f7945b93836795553968274a08cb87ede6ebe1
Alejandro Vera <avera@webclass.com>
2025-01-23 19:42:54 -0300

    crud

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,18 +1,44 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
 
 @Controller('cars')
 export class CarsController {
-    private cars =  ['Toyota','Honda','Jeep'];
+
+    constructor(
+        private readonly carsService:CarsService  
+    ){}
 
     @Get()
     getAllCars(){
-        return this.cars;
+        return this.carsService.findAll();
     }
     @Get(':id')
-    getCarById( @Param('id') id: string ){
+    getCarById( @Param('id',ParseIntPipe) id: number ){
+        const car   = this.carsService.findOneById(id)
+
+        if (!car) {
+            throw new NotFoundException(`Car with id ${id} not found`)
+        }
+        return car;
+    }
+
+    @Post()
+    createCar(@Body() body:any){
+        return body
+
+    }
+
+    @Patch(':id')
+    updateCar(@Body() body:any){
+        return body
+
+    }
+
+    @Delete(':id')
+    deleteCar(@Param('id',ParseIntPipe) id:number){
         return {
-            id:+id,
-            car:this.cars[+id]
+            method:'delete',
+            id
         }
     }
 }
